6d47ce08ef118076a09dfa33d520db3efd0a1220
Tomas <tl@premiato.org>
2018-03-08 20:16:30 +0100

    Most of blade formating done, first migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Cross or Check</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ...decisions that I am proud of!
+                    ...proud of my decisions!
                 </div>
 
                 <div class="links">
